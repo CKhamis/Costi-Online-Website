64489d3d9816cee7b5b4c4bf6e167138033fbc9a
Constantin Khamis <costikhamis@gmail.com>
2022-05-30 00:03:59 -0700

    Added final touches to first milestone!

--- src/main/java/com/costi/csw9/Config/WebSecurityConfig.java
+++ src/main/java/com/costi/csw9/Config/WebSecurityConfig.java
@@ -23,19 +23,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*http
+        http
                 .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/**")
                     .permitAll()
                 .anyRequest()
                 .authenticated().and()
-                .formLogin();*/
-        http
-                .requiresChannel(channel ->
-                        channel.anyRequest().requiresSecure())
-                .authorizeRequests(authorize ->
-                        authorize.anyRequest().permitAll());
+                .formLogin();
     }
 
     @Override
