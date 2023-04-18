56e07bfa21fa03c77d71c09553df52d12cc0fc34
Constantine Khamis <costikhamis@gmail.com>
2023-04-17 20:13:57 -0700

    Optimized implementation for AccountLog DAO. Reduced code, and even removed an entire class

--- src/main/java/com/costi/csw9/Repository/AccountLogRepository.java
+++ src/main/java/com/costi/csw9/Repository/AccountLogRepository.java
@@ -1,15 +1,10 @@
 package com.costi.csw9.Repository;
 
 import com.costi.csw9.Model.AccountLog;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-@Repository
-public interface AccountLogRepository {
-    AccountLog findById(Long id);
+public interface AccountLogRepository extends JpaRepository<AccountLog, Long> {
     List<AccountLog> findByUser(Long id);
-    @Modifying
-    void save(AccountLog log);
+
 }
