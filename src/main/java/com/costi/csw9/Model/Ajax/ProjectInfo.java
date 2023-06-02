ad70cb96d1032f10562917cca3d6a9cc93b4d0d9
Constantin Khamis <costikhamis@gmail.com>
2023-06-02 13:56:15 -0700

    Fully functional! This breaks clicking on links from home page though

--- src/main/java/com/costi/csw9/Model/Ajax/ProjectInfo.java
+++ src/main/java/com/costi/csw9/Model/Ajax/ProjectInfo.java
@@ -1,15 +1,44 @@
 package com.costi.csw9.Model.Ajax;
 
 public class ProjectInfo {
-    private String imagePath, subtitle, description, era;
+    private String imagePath, subtitle, description, era, title, id, type;
+    private boolean discontinued;
     private String[] repositoryLinks;
 
-    public ProjectInfo(String imagePath, String subtitle, String description, String era, String[] repositoryLinks) {
+    public ProjectInfo(String era, String type, String id, String imagePath, String title, boolean discontinued, String subtitle, String description, String[] repositoryLinks) {
         this.imagePath = imagePath;
         this.subtitle = subtitle;
         this.description = description;
         this.era = era;
+        this.title = title;
+        this.id = id;
         this.repositoryLinks = repositoryLinks;
+        this.type = type;
+        this.discontinued = discontinued;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getImagePath() {
@@ -50,5 +79,13 @@ public class ProjectInfo {
 
     public void setEra(String era) {
         this.era = era;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
