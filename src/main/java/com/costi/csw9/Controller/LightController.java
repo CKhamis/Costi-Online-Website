b6ee097e5d7ca7371b6d8e5069868bab898e0599
Constantin Khamis <costikhamis@gmail.com>
2023-06-10 12:31:25 -0700

    Enable/Disable now works, although the behaviors need to be fully thought out

--- src/main/java/com/costi/csw9/Controller/LightController.java
+++ src/main/java/com/costi/csw9/Controller/LightController.java
@@ -100,4 +100,87 @@ public class LightController {
         }
     }
 
+    @PostMapping("/api/v1/LED/{id}/Delete")
+    public ResponseEntity<String> deleteLight(@PathVariable Long id, Principal principal) {
+        User user = getCurrentUser(principal);
+        if(user.getRole().equals(UserRole.ADMIN) || getCurrentUser(principal).getRole().equals(UserRole.OWNER)){
+            try {
+                lightService.deleteLightById(id);
+                return ResponseEntity.ok("Light deleted successfully");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting light: " + e.getMessage());
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+    @PostMapping("/api/v1/LED/{id}/Enable")
+    public ResponseEntity<String> enableLight(@PathVariable Long id, Principal principal) {
+        User user = getCurrentUser(principal);
+        if(user.getRole().equals(UserRole.ADMIN) || getCurrentUser(principal).getRole().equals(UserRole.OWNER)){
+            try {
+                Light light = lightService.getLightById(id);
+                light.setEnabled(true);
+                lightService.saveLight(light);
+                return ResponseEntity.ok("Light enabled successfully");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error enabling light: " + e.getMessage());
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+    @PostMapping("/api/v1/LED/{id}/Disable")
+    public ResponseEntity<String> disableLight(@PathVariable Long id, Principal principal) {
+        User user = getCurrentUser(principal);
+        if(user.getRole().equals(UserRole.ADMIN) || getCurrentUser(principal).getRole().equals(UserRole.OWNER)){
+            try {
+                Light light = lightService.getLightById(id);
+                light.setEnabled(false);
+                lightService.saveLight(light);
+                return ResponseEntity.ok("Light disabled successfully");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error disabling light: " + e.getMessage());
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+    @PostMapping("/api/v1/LED/{id}/Publish")
+    public ResponseEntity<String> publishLight(@PathVariable Long id, Principal principal) {
+        User user = getCurrentUser(principal);
+        if(user.getRole().equals(UserRole.ADMIN) || getCurrentUser(principal).getRole().equals(UserRole.OWNER)){
+            try {
+                Light light = lightService.getLightById(id);
+                light.setPublic(true);
+                lightService.saveLight(light);
+                return ResponseEntity.ok("Light published successfully");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error publishing light: " + e.getMessage());
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+    @PostMapping("/api/v1/LED/{id}/Private")
+    public ResponseEntity<String> privateLight(@PathVariable Long id, Principal principal) {
+        User user = getCurrentUser(principal);
+        if(user.getRole().equals(UserRole.ADMIN) || getCurrentUser(principal).getRole().equals(UserRole.OWNER)){
+            try {
+                Light light = lightService.getLightById(id);
+                light.setPublic(false);
+                lightService.saveLight(light);
+                return ResponseEntity.ok("Light privated successfully");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error privating light: " + e.getMessage());
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
 }
