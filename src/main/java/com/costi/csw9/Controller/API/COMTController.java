f625606e4c4e2b67d910a2f2ec6029f32acd1a79
Constantin Khamis <costikhamis@gmail.com>
2023-07-04 15:17:00 -0700

    COMT Announcement page is able to populate with announcements and stats. Unable to do anything yet. Happy 4th!

--- src/main/java/com/costi/csw9/Controller/API/COMTController.java
+++ src/main/java/com/costi/csw9/Controller/API/COMTController.java
@@ -8,7 +8,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/management")
@@ -32,6 +36,42 @@ public class COMTController {
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/announcement/analytics")
+    @ResponseBody
+    public Map<String, Object> getAnnouncementAnalytics(){
+        Map<String, Object> jsonResponse = new HashMap<>();
+        List<Announcement> allAnnouncements = comtService.findAllAnnouncements();
+        jsonResponse.put("totalAnnouncements", allAnnouncements.size());
+
+        int numEnabled = 0, numDisabled = 0, numBodyCharacters = 0;
+        LocalDateTime oldest = LocalDateTime.MAX, newest = LocalDateTime.MIN;
+        for(Announcement announcement : allAnnouncements){
+            if(announcement.isEnable()){
+                numEnabled++;
+            }else{
+                numDisabled++;
+            }
+
+            if(announcement.getDate().isBefore(oldest)){
+                oldest = announcement.getDate();
+            }
+
+            if(announcement.getDate().isAfter(newest)){
+                newest = announcement.getDate();
+            }
+
+            numBodyCharacters += announcement.getBody().length();
+        }
+
+        jsonResponse.put("enabledCount", numEnabled);
+        jsonResponse.put("disabledCount", numDisabled);
+        jsonResponse.put("oldestAnnouncement", ChronoUnit.DAYS.between(oldest, LocalDateTime.now()) + "d");
+        jsonResponse.put("newestAnnouncement", ChronoUnit.DAYS.between(newest, LocalDateTime.now()) + "d");
+        jsonResponse.put("averageBodyLength", numBodyCharacters / allAnnouncements.size());
+
+        return jsonResponse;
     }
 
     @GetMapping("/announcement/all")
