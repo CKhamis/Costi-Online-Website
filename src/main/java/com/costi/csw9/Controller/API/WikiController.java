4c6740ea60857b3bfceeb2215a5736fc6e469165
Constantin Khamis <costikhamis@gmail.com>
2023-08-27 00:09:55 -0700

    Fixed very weird git problem. Able to create new Wiki Pages. Forms reset upon errors.

--- src/main/java/com/costi/csw9/Controller/API/WikiController.java
+++ src/main/java/com/costi/csw9/Controller/API/WikiController.java
@@ -2,6 +2,7 @@ package com.costi.csw9.Controller.API;
 
 import com.costi.csw9.Model.DTO.ResponseMessage;
 import com.costi.csw9.Model.DTO.WikiRequest;
+import com.costi.csw9.Model.FlashMessage;
 import com.costi.csw9.Model.Post;
 import com.costi.csw9.Model.User;
 import com.costi.csw9.Model.WikiPage;
@@ -10,10 +11,8 @@ import com.costi.csw9.Service.WikiService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -57,18 +56,24 @@ public class WikiController {
         }
     }
 
-    @GetMapping("/save")
-    public ResponseEntity<?> save(@RequestBody @Valid WikiRequest request, Principal principal) {
+    @PostMapping("/save")
+    public String save(@Valid WikiRequest request, Principal principal, RedirectAttributes redirectAttributes) {
         try {
-            return ResponseEntity.ok(wikiService.save(request, getCurrentUser(principal)));
+            wikiService.save(request, getCurrentUser(principal));
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Wiki submission sent", "Please allow a few days for Costi Online moderators to review your wiki page", FlashMessage.Status.SUCCESS));
+            return "redirect:/Wiki";
         } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage("ID of wiki page is invalid", ResponseMessage.Severity.LOW, e.getMessage()));
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Error editing wiki page", "ID of wiki page is invalid", FlashMessage.Status.DANGER));
+            return "redirect:/Wiki/Create";
         } catch (AccessDeniedException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ResponseMessage("Wiki page Not Public", ResponseMessage.Severity.LOW, e.getMessage()));
-        } catch (Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseMessage("Error Getting wiki page", ResponseMessage.Severity.MEDIUM, e.getMessage()));
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Invalid permissions", "Please sign in with account used to create the wiki page", FlashMessage.Status.DANGER));
+            return "redirect:/Wiki/Create";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("OOOOPS! Generic error (awwww man)", "Perhaps the title isn't unique?", FlashMessage.Status.DANGER));
+            return "redirect:/Wiki/Create";
         }
     }
+
 
     private User getCurrentUser(Principal principal) throws Exception{
         if (principal == null) {
