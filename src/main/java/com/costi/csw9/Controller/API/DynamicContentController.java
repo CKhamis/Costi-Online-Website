34e8bf28212f79e60562feea2da3d28a6c1f2f9a
Constantine Khamis <costikhamis@gmail.com>
2024-02-08 13:15:22 -0800

    Further developed front end for content tools. Can now interact with back end to retrieve a list of currently created content

--- src/main/java/com/costi/csw9/Controller/API/DynamicContentController.java
+++ src/main/java/com/costi/csw9/Controller/API/DynamicContentController.java
@@ -1,20 +1,13 @@
 package com.costi.csw9.Controller.API;
 
-import com.costi.csw9.Model.AccountLog;
 import com.costi.csw9.Model.DTO.ResponseMessage;
 import com.costi.csw9.Model.DynamicContent;
-import com.costi.csw9.Model.User;
-import com.costi.csw9.Repository.AccountLogRepository;
 import com.costi.csw9.Service.DynamicContentService;
-import com.costi.csw9.Service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequestMapping("/api/content")
